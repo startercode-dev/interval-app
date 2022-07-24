f90f171ca6193a9d7e3b3ec9df3c4fe4b2d45210
hpk <hpk@hpks-MacBook-Pro.local>
2022-07-24 12:38:31 -0700

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+const getTotalTime = (e) => {
+    e.preventDefault();
+    const inputTime = $("#inputTime").val() * 1;
+    const inputRest = $("#inputRest").val() * 1;
+    const inputExercise = $("#inputExercise").val() * 1;
+    const inputSet = $("#inputSet").val() * 1;
+    const inputSetRest = $("#inputSetRest").val() * 1;
+
+    const totalTime =
+        (inputTime + inputRest) * inputExercise + inputSet * inputSetRest;
+
+    const minutes = Math.floor(totalTime / 60);
+    const seconds = (totalTime % 60).toString();
+    const time = `${minutes}:${seconds.padStart(2, "0")}`;
+
+    $(".total-time span").text(time);
+};
+
+$(".btn").click((e) => getTotalTime(e));
