cd6635c5cac387b2a78747a4632dccf6a102ad08
ronaldthecoder <ronaldchan96@gmail.com>
2022-07-24 12:46:19 -0700

    CDs nuts

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const getTotalTime = (e) => {
-    e.preventDefault();
-    const inputTime = $("#inputTime").val() * 1;
-    const inputRest = $("#inputRest").val() * 1;
-    const inputExercise = $("#inputExercise").val() * 1;
-    const inputSet = $("#inputSet").val() * 1;
-    const inputSetRest = $("#inputSetRest").val() * 1;
+  e.preventDefault();
+  const inputTime = $("#inputTime").val() * 1;
+  const inputRest = $("#inputRest").val() * 1;
+  const inputExercise = $("#inputExercise").val() * 1;
+  const inputSet = $("#inputSet").val() * 1;
+  const inputSetRest = $("#inputSetRest").val() * 1;
 
-    const totalTime =
-        (inputTime + inputRest) * inputExercise + inputSet * inputSetRest;
+  const totalTime =
+    (inputTime + inputRest) * inputExercise + inputSet * inputSetRest;
 
-    const minutes = Math.floor(totalTime / 60);
-    const seconds = (totalTime % 60).toString();
-    const time = `${minutes}:${seconds.padStart(2, "0")}`;
+  const minutes = Math.floor(totalTime / 60);
+  const seconds = (totalTime % 60).toString();
+  const time = `${minutes}:${seconds.padStart(2, "0")}`;
 
-    $(".total-time span").text(time);
+  $(".total-time span").text(time);
 
-    console.log('Testing Edit')
+  console.log("Testing Edit 2");
 };
 
 $(".btn").click((e) => getTotalTime(e));
