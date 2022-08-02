c1319e577056baf46f4db38fcb2fde2c4774e26b
hpk <jkuang41@gmail.com>
2022-08-01 18:50:01 -0700

    init input algo complete

---
+++ js/model.js
@@ -0,0 +1,14 @@
+export const state = {
+    setting: {},
+};
+
+export const updateTime = () => {
+    state.setting = {
+        numExercises: $("#numExercises").val() * 1,
+        timeExercise: $("#timeExercise").val() * 1,
+        restExercise: $("#restExercise").val() * 1,
+        numSets: $("#numSets").val() * 1,
+        restSet: $("#restSet").val() * 1,
+    };
+    // console.log(state.setting);
+};
