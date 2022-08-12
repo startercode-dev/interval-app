bb9f316f7a6ae44b5266d7658ce923a7d75a1d4c
hpk992 <jkuang41@gmail.com>
2022-08-11 21:52:49 -0700

    1.0.3-alpha/Refactor, small css update

--- js/controller.js
+++ js/controller.js
@@ -7,105 +7,76 @@ import infoView from "./views/infoView.js";
 import { formatTime, calcTime } from "./helper.js";
 
 // TIME REMAIN
+let totalTimer;
 const totalCountdown = function () {
     let totalTime = calcTime();
     const tick = () => {
         totalTime--;
-        $(".timer-clock__remain span").text(formatTime(totalTime));
+        $(`.timer-clock__remain span`).text(formatTime(totalTime));
         if (totalTime < 1) {
-            clearInterval(timer);
+            clearInterval(totalTimer);
         }
     };
 
-    let timer = setInterval(tick, 1000);
-
-    $(".btn--pause").click(() => {
-        if (model.state.isPaused) {
-            clearInterval(timer);
-        }
-    });
+    totalTimer = setInterval(tick, 1000);
 
     $(".btn--resume").click(() => {
-        clearInterval(timer);
+        clearInterval(totalTimer);
         if (!model.state.isPaused) {
-            timer = setInterval(tick, 1000);
+            totalTimer = setInterval(tick, 1000);
         }
-    });
-
-    $(".btn--reset").click(() => {
-        clearInterval(timer);
-        model.updateTime();
-        const total = calcTime();
-        $(".timer-clock__remain span").text(formatTime(total));
     });
 };
 
 // MAIN COUNT
+let mainTimer;
 const count = function (t) {
     return new Promise((resolve, reject) => {
         $(".timer-clock__label").text(formatTime(t));
         const tick = () => {
             t--;
             $(".timer-clock__label").text(formatTime(t));
-            if (t < 1) {
-                clearInterval(timer);
+            if (t === 0) {
+                clearInterval(mainTimer);
                 resolve();
             }
         };
 
-        let timer = setInterval(tick, 1000);
-
-        $(".btn--pause").click(() => {
-            if (model.state.isPaused) {
-                clearInterval(timer);
-            }
-        });
+        mainTimer = setInterval(tick, 1000);
 
         $(".btn--resume").click(() => {
+            clearInterval(mainTimer);
             if (!model.state.isPaused) {
-                clearInterval(timer);
-                timer = setInterval(tick, 1000);
+                mainTimer = setInterval(tick, 1000);
             }
-        });
-
-        $(".btn--reset").click(() => {
-            clearInterval(timer);
-            model.updateTime();
-            $(".timer-clock__label").text(
-                `${formatTime(model.state.setting.timeExercise)}`
-            );
-            reject("reset");
         });
     });
 };
 
 const mainCountdown = async function () {
-    try {
-        const { numExercises, timeExercise, restExercise, numSets, restSet } =
-            model.state.setting;
+    const { numExercises, timeExercise, restExercise, numSets, restSet } =
+        model.state.setting;
 
-        let currNumS = 0;
-        let currNumE = 0;
+    let currNumS = 0;
+    let currNumE = 0;
 
-        for (let i = 0; i < numSets; i++) {
-            currNumS++;
-            currNumE = 0;
+    for (let i = 0; i < numSets; i++) {
+        currNumS++;
+        currNumE = 0;
 
-            for (let j = 0; j < numExercises; j++) {
-                currNumE++;
-                await count(timeExercise);
-                if (currNumE < numExercises && restExercise > 0) {
-                    await count(restExercise);
-                }
-            }
-
-            if (currNumS < numSets && restSet > 0) {
-                await count(restSet);
+        for (let j = 0; j < numExercises; j++) {
+            currNumE++;
+            await count(timeExercise);
+            if (currNumE < numExercises && restExercise > 0) {
+                await count(restExercise);
             }
         }
-    } catch (err) {
-        console.log(err);
+
+        if (currNumS < numSets && restSet > 0) {
+            await count(restSet);
+        }
     }
+    $(".btn--pause").prop("disabled", true);
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -134,6 +105,9 @@ const controlStart = function (e) {
 const controlPause = function (e) {
     e.preventDefault();
     model.state.isPaused = true;
+
+    clearInterval(totalTimer);
+    clearInterval(mainTimer);
 };
 
 const controlResume = function (e) {
@@ -147,6 +121,11 @@ const controlReset = function (e) {
     e.target.disabled = true;
     model.state.isPaused = false;
     model.state.isStopped = true;
+
+    clearInterval(mainTimer);
+    clearInterval(totalTimer);
+    timerView.render(model.state.setting);
+    infoView.render(model.state.setting);
 };
 
 const init = function () {
