83f9bcfdef5574553622fe516eaebda1175310ef
hpk992 <jkuang41@gmail.com>
2022-08-16 12:16:05 -0700

    1.0.4-alpha/Refactor, fixed small bugs

--- js/views/timerView.js
+++ js/views/timerView.js
@@ -4,6 +4,16 @@ import View from "./View.js";
 import { formatTime } from "../helper.js";
 
 class TimerView extends View {
+    totalTimer;
+    totalTime;
+    mainTimer;
+
+    _generateView() {
+        const total = this.getTotal();
+        $(".timer-clock__remain span").text(formatTime(total));
+        $(".timer-clock__label").text(`${formatTime(this._data.timeExercise)}`);
+    }
+
     addHandlerPauseBtn(handler) {
         $(".btn--pause").click((e) => {
             handler(e);
@@ -12,6 +22,9 @@ class TimerView extends View {
             e.target.classList.add("hidden");
             $(".btn--resume").removeClass("hidden");
             $(".btn--resume").prop("disabled", false);
+
+            clearInterval(this.totalTimer);
+            clearInterval(this.mainTimer);
         });
     }
 
@@ -23,6 +36,14 @@ class TimerView extends View {
             e.target.classList.add("hidden");
             $(".btn--pause").removeClass("hidden");
             $(".btn--pause").prop("disabled", false);
+
+            this.totalTimer = setInterval(() => {
+                this.totalTime--;
+                $(`.timer-clock__remain span`).text(formatTime(this.totalTime));
+                if (this.totalTime < 1) {
+                    clearInterval(this.totalTimer);
+                }
+            }, 1000);
         });
     }
 
@@ -37,13 +58,70 @@ class TimerView extends View {
             $(".btn--resume").addClass("hidden");
 
             $(".input-form input").prop("disabled", false);
+
+            clearInterval(this.totalTimer);
+            clearInterval(this.mainTimer);
         });
     }
 
-    _generateView() {
-        const total = this.getTotal();
-        $(".timer-clock__remain span").text(formatTime(total));
-        $(".timer-clock__label").text(`${formatTime(this._data.timeExercise)}`);
+    totalCountdown() {
+        this.totalTime = this.getTotal();
+
+        this.totalTimer = setInterval(() => {
+            this.totalTime--;
+            $(`.timer-clock__remain span`).text(formatTime(this.totalTime));
+            if (this.totalTime < 1) {
+                clearInterval(this.totalTimer);
+            }
+        }, 1000);
+    }
+
+    async mainCountdown() {
+        const { numExercises, timeExercise, restExercise, numSets, restSet } =
+            this._data;
+
+        let currNumS = 0;
+        let currNumE = 0;
+
+        for (let i = 0; i < numSets; i++) {
+            currNumS++;
+            currNumE = 0;
+
+            for (let j = 0; j < numExercises; j++) {
+                currNumE++;
+                await this.count(timeExercise);
+                if (currNumE < numExercises && restExercise > 0) {
+                    await this.count(restExercise);
+                }
+            }
+
+            if (currNumS < numSets && restSet > 0) {
+                await this.count(restSet);
+            }
+        }
+        $(".btn--pause").prop("disabled", true);
+    }
+
+    count(t) {
+        return new Promise((resolve) => {
+            $(".timer-clock__label").text(formatTime(t));
+            const tick = () => {
+                t--;
+                $(".timer-clock__label").text(formatTime(t));
+
+                if (t === 0) {
+                    clearInterval(this.mainTimer);
+                    resolve();
+                }
+            };
+
+            this.mainTimer = setInterval(tick, 1000);
+
+            $(".btn--resume").click(() => {
+                clearInterval(this.mainTimer);
+                this.mainTimer = setInterval(tick, 1000);
+            });
+        });
     }
 }
 
