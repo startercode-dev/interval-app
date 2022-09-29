101feb298b844875023abdd45c31c55304b0de27
hpk992 <jkuang41@gmail.com>
2022-09-28 18:38:46 -0700

    0.2/converted into nodejs app

--- models/presetModel.js
+++ models/presetModel.js
@@ -1,26 +1,47 @@
 const mongoose = require('mongoose');
 
-const presetSchema = new mongoose.Schema({
-    numExercise: {
-        type: Number,
-        required: true,
-        default: 1,
+const presetSchema = new mongoose.Schema(
+    {
+        numExercise: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+
+        timeExercise: {
+            type: Number,
+            required: [true, 'must have exercise time'],
+        },
+
+        restExercise: Number,
+
+        numSet: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+
+        restSet: Number,
+
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'must belong to a user'],
+        },
     },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
-    timeExercise: {
-        type: Number,
-        required: [true, 'must have exercise time'],
-    },
+presetSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'user',
+        select: 'email',
+    });
 
-    restExercise: Number,
-
-    numSet: {
-        type: Number,
-        required: true,
-        default: 1,
-    },
-
-    restSet: Number,
+    next();
 });
 
 const Preset = mongoose.model('preset', presetSchema);
